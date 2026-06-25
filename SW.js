150d59f3adaca2379d0222e28b5a2240d15fb79c
samakinpelu2008-dot <samakinpelu2008@gmail.com>
2026-06-25 09:20:42 +0100

    Add service worker for caching assets

---
+++ SW.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'syrix-arena-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './audio.js',
+  './map.js',
+  './enemies.js',
+  './action.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
